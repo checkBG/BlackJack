a41aa1e6275e152a9cfbf599e89d6f62d7e112e5
Alexander Chernyshov <alcherng2015@gmail.com>
2024-07-12 19:58:46 +0300

    Refactored the logic of the game

--- src/main/kotlin/com/blackjack/BlackJackGame.kt
+++ src/main/kotlin/com/blackjack/BlackJackGame.kt
@@ -10,13 +10,21 @@ class Game(private val user: User, private val dealer: Dealer) {
         val deck = Deck()
     }
 
-    private fun result(player: Int, dealer: Int): String {
+    private fun result(): String {
         return when {
-            (((player > dealer) && (player <= 21)) || (dealer > 21)) -> "You ${"won".changeColor(color = Color.BRIGHT_RED)}! Congratulations!!!"
+            (dealer.hand.costOfHand == 21 && dealer.hand.cardsInHand.size == 2 && user.hand.costOfHand != 21) -> "The dealer has blackjack, you have lost"
 
-            (dealer == 0) -> "You lost, you had too much, the dealer didn't even take the cards!"
+            (((user.hand.costOfHand > dealer.hand.costOfHand) && (user.hand.costOfHand <= 21)) || (dealer.hand.costOfHand > 21) || user.hand.costOfHand == 21) -> "You ${
+                "won".changeColor(
+                    color = Color.BRIGHT_RED
+                )
+            }! Congratulations!!!"
 
-            (player < dealer) || (player > 21) -> {
+            (dealer.hand.cardsInHand.size == 1) -> "You lost, you had too much, the dealer didn't even take the cards!" // одна карта по умолчанию,
+            // потому что по правилам игры крупье раздаёт всем по две карты, а себе только одну, но в случае перебора игрока или набора им 21 очка дилер не берёт карты,
+            // а значит если игрок брал одну карту, то проиграл, потому что проверка на 21 было выше
+
+            (user.hand.costOfHand < dealer.hand.costOfHand) || (user.hand.costOfHand > 21) -> {
                 val smile = listOf("(｡•́︿•̀｡)", "(っ◞‸◟c)", "ಥ_ಥ").random()
                 "You ${"lost".changeColor(color = Color.BRIGHT_WHITE)}! ${smile.changeColor(color = Color.BRIGHT_WHITE)} Oops, it happens"
             }
@@ -26,14 +34,29 @@ class Game(private val user: User, private val dealer: Dealer) {
     }
 
     fun play() {
-        while (user.hand.costOfHand < 21) {
+        while (user.hand.costOfHand <= 21) {
             println(user.information())
             println("Another card? Yes/No: ")
 
-            if (readln().lowercase() in listOf("y", "yes", "да", "д", "yep", "yeah")) {
+            if (readln().lowercase() in listOf(
+                    "карту",
+                    "ещё",
+                    "да",
+                    "д",
+                    "y",
+                    "yes",
+                    "yep",
+                    "yeah",
+                    "card",
+                    "hit me"
+                ) && user.hand.costOfHand < 21
+            ) {
                 user.takeCard()
-            } else if (user.hand.costOfHand <= 21) {
-                dealer.artificialIntelligence()
+            } else if (
+                user.hand.costOfHand < 21 ||
+                (user.hand.costOfHand == 21 && dealer.hand.costOfHand in 10..11)
+            ) {
+                dealer.setOfCardsByDealer()
                 break
             } else {
                 break
@@ -42,7 +65,7 @@ class Game(private val user: User, private val dealer: Dealer) {
 
         println(
             """
-        |${result(user.hand.costOfHand, dealer.hand.costOfHand)}
+        |${result()}
         |${user.information()}
         |
         |${dealer.information()}
--- src/main/kotlin/com/blackjack/players/Dealer.kt
+++ src/main/kotlin/com/blackjack/players/Dealer.kt
@@ -1,8 +1,5 @@
 package com.blackjack.players
 
-import kotlin.random.Random
-import kotlin.random.nextInt
-
 class Dealer(nameDealer: String = "The dealer") : Player(nameDealer) {
     override val pronoun: String = "his"
 
@@ -15,40 +12,12 @@ class Dealer(nameDealer: String = "The dealer") : Player(nameDealer) {
         takeCard()
     }
 
-    fun artificialIntelligence() {
+    fun setOfCardsByDealer() {
+        println("Myself...")
         println("$name starts taking cards")
-//        defaultDistribution()
 
-        while (true) {
-
-            when (hand.costOfHand) {
-                in 0..11 -> takeCard()
-
-                in 12..15 -> {
-                    when (Random.nextInt(1..2)) {
-                        1 -> takeCard()
-                        else -> return
-                    }
-                }
-
-                in 16..17 -> {
-                    when (Random.nextInt(1..10)) {
-                        1 -> takeCard()
-                        else -> return
-                    }
-                }
-
-                18 -> {
-                    when (Random.nextInt(1..100)) {
-                        in 1..4 -> takeCard()
-                        else -> return
-                    }
-                }
-
-                in 19..21 -> return
-
-                else -> return
-            }
+        while (hand.costOfHand < 17) {
+            takeCard()
         }
     }
 }
